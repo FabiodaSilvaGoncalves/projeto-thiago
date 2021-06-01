471cc0b7343579619d1d488ea6c8527a4116e14c
FabioSilva17 <>
2021-06-01 01:44:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,125 @@
+var parar = 0;
+while(parar == 0){
+
+console.log("")
+console.log("Bem vindo a music.inc")
+console.log("")
+console.log("Cantores disponiveis:")
+console.log("")
+console.log("Avril lavigne")
+console.log("")
+console.log("Mc lipi")
+console.log("")
+console.log("Jorge e matheus")
+console.log("")
+var expr = prompt("Escolha um cantor")
+console.log("")
+
+switch(expr){
+
+case 'Avril lavigne' :
+
+for(var expf = prompt("Escolha uma musica dela"); expf <= 3; expf++){console.log(expf)}
+console.log("")
+console.log("obrigado por escolher uma musica conosco")
+console.log("")
+
+var ointb = prompt("gostaria de escolher outra?")
+
+if(ointb == 'sim'){
+
+console.log("")
+
+}
+
+else {
+
+console.log("seu numero 55")
+var expz = prompt("confime seu numero")
+
+do {
+  console.log("obrigado pela preferencia")
+} while(expz === 5)
+
+
+var ouvintes = 20;
+console.log("com vc temos")
+console.log("ouvintes:")
+console.log(ouvintes + 1)
+
+
+break}
+
+break
+
+ case 'Mc lipi' :
+
+for(var expf = prompt("Escolha uma musica dela"); expf <= 3; expf++){console.log(expf)}
+console.log("")
+console.log("obrigado por escolher uma musica conosco")
+console.log("")
+
+var ointb = prompt("gostaria de escolher outra?")
+
+if(ointb == 'sim'){
+
+console.log("")
+
+}
+
+else {
+
+console.log("seu numero 55")
+var expz = prompt("confime seu numero")
+
+do {
+  console.log("obrigado pela preferencia")
+} while(expz === 5)
+
+
+var ouvintes = 20;
+console.log("com vc temos")
+console.log("ouvintes:")
+console.log(ouvintes + 1)
+
+
+break}
+
+ break
+
+case 'Jorge e Matheus' :
+
+for(var expf = prompt("Escolha uma musica dela"); expf <= 3; expf++){console.log(expf)}
+console.log("")
+console.log("obrigado por escolher uma musica conosco")
+console.log("")
+
+var ointb = prompt("gostaria de escolher outra?")
+
+if(ointb == 'sim'){
+
+console.log("")
+
+}
+
+else {
+
+console.log("seu numero 55")
+var expz = prompt("confime seu numero")
+
+do {
+  console.log("obrigado pela preferencia")
+} while(expz === 5)
+
+
+var ouvintes = 20;
+console.log("com vc temos")
+console.log("ouvintes:")
+console.log(ouvintes + 1)
+
+
+break}
+
+
+}
+}
